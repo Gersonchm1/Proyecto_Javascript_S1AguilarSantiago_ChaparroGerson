b4d62b077d71c8e1013ad4e3200fd09268656dde
Santiagoaghhh <santaguilar17@gmail.com>
2025-05-21 22:15:01 -0500

    feat: :art: adding css and js

--- login-admin/scripts/script.js
+++ login-admin/scripts/script.js
@@ -1,15 +1,16 @@
 document.getElementById('login').addEventListener('click', verificacion);
 
 async function cambiarpg() {
-    window.location.replace('../Proyecto_Javascript_S1AguilarSantiago_ChaparroGerson/user-mainPage/index.html');
+    window.location.href('../user-mainPage/index.html');
 }
 
 async function verificacion() {
     const inputEmail = document.querySelector('#email').value;
     const inputPassword = document.querySelector('#password').value;
 
-    if (inputEmail === 'adminprueba@example.com' && inputPassword === '12345') {
+    if (inputEmail == 'example@gmail.com' && inputPassword == '12345') {
         cambiarpg();
+        alert('CAMBIANDO')
     } else {
         alert('ERROR EN LA AUTENTICACIÓN');
         alert('Contraseña por defecto: 12345');
---
+++ user-signin/scripts/script.js
@@ -0,0 +1,17 @@
+document.getElementById('login').addEventListener('click', verificacion);
+
+async function cambiarpg() {
+    window.location.replace('../user-mainPage/index.html');
+}
+
+async function verificacion() {
+    const inputEmail = document.querySelector('#email').value;
+    const inputPassword = document.querySelector('#password').value;
+
+    if (inputEmail == 'example@gmail.com' && inputPassword == '12345') {
+        cambiarpg();
+    } else {
+        alert('ERROR EN LA AUTENTICACIÓN');
+        alert('Contraseña por defecto: 12345');
+    }
+}
