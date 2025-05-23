0d359b1d9cf31a9064b59dbc3119ff05ec5589d2
SantiagoAghh <santaguilar17@gmail.com>
2025-05-22 23:39:35 -0500

    feat: :lipstick: Adding responsive pages

---
+++ admin-mainPage/script/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btnEscuderias = document.querySelector('.container--escuderias__titulo button');
+    const btnVehiculos = document.querySelector('.container--vehiculos__titulo button');
+    const btnPilotos = document.querySelector('.container--pilotos__titulo button');
+    const btnCircuitos = document.querySelector('.container--circuitos__titulo button');
+
+    btnEscuderias.addEventListener('click', () => {
+        window.location.href = '../user';
+    });
+
+    btnVehiculos.addEventListener('click', () => {
+        window.location.href = '../vehiculos-admin/index.html';
+    });
+
+    btnPilotos.addEventListener('click', () => {
+        window.location.href = '../avatar-admin/index.html';
+    });
+
+    btnCircuitos.addEventListener('click', () => {
+        window.location.href = '../mapas-admin/index.html';
+    });
+});
